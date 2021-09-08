16bcfae8a2061aa1fbe27555c6b676ba2c388198
amywieliczka <amywieliczka@gmail.com>
2021-09-08 12:43:21 -0700

    Moved temp into cache_retry

--- calisphere/cache_retry.py
+++ calisphere/cache_retry.py
@@ -15,6 +15,7 @@ import pickle
 import hashlib
 import json
 import itertools
+from typing import Dict, List, Tuple
 
 requests.packages.urllib3.disable_warnings()
 
@@ -226,3 +227,102 @@ def SOLR_select_nocache(**kwargs):
     kwargs.update(SOLR_DEFAULTS)
     sr = SOLR(**kwargs)
     return sr
+
+
+FieldName = str
+Order = str
+FilterValues = list
+FilterField = Dict[FieldName, FilterValues]
+Filters = List[FilterField]
+
+
+def query_encode(query_string: str = None, 
+                 filters: Filters = None,
+                 exclude: Filters = None,
+                 sort: Tuple[FieldName, Order] = None,
+                 start: int = None,
+                 rows: int = 0,
+                 result_fields: List[str] = None,
+                 facets: List[str] = None,
+                 facet_sort: dict = None):
+
+    es_params = {}
+
+    es_query = es_filters = es_exclude = None
+
+    if query_string:
+        es_query = [{
+            "query_string": {
+                "query": query_string
+            }
+        }]
+
+    if filters:
+        es_filters = [{
+            'terms': {k: v} for k, v in f.items()
+        } for f in filters]
+
+    if exclude:
+        es_exclude = [{
+            'terms': {k: v} for k, v in e.items()
+        } for e in exclude]
+
+    if es_query or es_filters or es_exclude:
+        es_params['query'] = {'bool': {}}
+        if es_query:
+            es_params['query']['bool']['must'] = es_query
+        if es_filters:
+            es_params['query']['bool']['filter'] = es_filters
+        if es_exclude:
+            es_params['query']['bool']['must_not'] = es_exclude
+
+        # unsure if this is an unnecessary optimization:
+        # https://discuss.elastic.co/t/filter-performance-difference-bool-vs-terms/59928
+        if len(es_params['query']['bool']) == 1:
+            if 'must' in es_params['query']['bool']:
+                es_params['query'] = es_query[0]
+            elif ('filter' in es_params['query']['bool'] and 
+                  len(es_params['query']['bool']['filter']) == 1):
+                es_params['query'] = es_filters[0]
+
+    if facets:
+        exceptions = ['collection_ids', 'repository_ids', 'campus_ids']
+        aggs = {}
+        for facet in facets:
+            if facet in exceptions or facet[-8:] == '.keyword':
+                field = facet
+            else:
+                field = f'{facet}.keyword'
+
+            aggs[facet] = {
+                "terms": {
+                    "field": field,
+                    "size": 10000
+                }
+            }
+
+            if facet_sort:
+                aggs[facet]["terms"]["order"] = facet_sort
+        # regarding 'size' parameter here and getting back all the facet values
+        # please see: https://github.com/elastic/elasticsearch/issues/18838
+
+        es_params.update({"aggs": aggs})
+
+    if result_fields:
+        es_params.update({"_source": result_fields})
+
+    # if sort:
+    #     es_params.update({
+    #         "sort": [{
+    #             sort[0]: {"order": sort[1]}
+    #         }]
+    #     })
+    
+    es_params.update({'size': rows})
+    if start:
+        es_params.update({'from': start})
+    return es_params
+
+
+def search_index(query):
+    return ES_search(query_encode(**query))
